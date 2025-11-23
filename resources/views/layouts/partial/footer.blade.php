0549c75efa0fb7439535934bfa025984f5860c8d
fahmi-blip <fahmisyhb9@gmail.com>
2025-11-23 16:28:03 +0700

    baru1

--- resources/views/layouts/partial/footer.blade.php
+++ resources/views/layouts/partial/footer.blade.php
@@ -1,61 +1,70 @@
-<footer class="pt-4">
-          <div class="w-full px-6 mx-auto">
-            <div class="flex flex-wrap items-center -mx-3 lg:justify-between">
-              <div
-                class="w-full max-w-full px-3 mt-0 mb-6 shrink-0 lg:mb-0 lg:w-1/2 lg:flex-none">
-                <div
-                  class="text-sm leading-normal text-center text-slate-500 lg:text-left">
-                  ©
-                  <script>
-                    document.write(new Date().getFullYear() + ",");
-                  </script>
-                  made with <i class="fa fa-heart"></i> by
-                  <a
-                    href="https://www.creative-tim.com"
-                    class="font-semibold text-slate-700"
-                    target="_blank"
-                    >Creative Tim</a
-                  >
 
-                  for a better web.
-                  <span class="w-full"> Distributed by ❤️ ThemeWagon </span>
-                </div>
-              </div>
-              <div
-                class="w-full max-w-full px-3 mt-0 shrink-0 lg:w-1/2 lg:flex-none">
-                <ul
-                  class="flex flex-wrap justify-center pl-0 mb-0 list-none lg:justify-end">
-                  <li class="nav-item">
-                    <a
-                      href="#!"
-                      class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500"
-                      >Creative Tim</a
-                    >
-                  </li>
-                  <li class="nav-item">
-                    <a
-                      href="#!"
-                      class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500"
-                      >About Us</a
-                    >
-                  </li>
-                  <li class="nav-item">
-                    <a
-                      href="#!"
-                      class="block px-4 pt-0 pb-1 text-sm font-normal transition-colors ease-soft-in-out text-slate-500"
-                      >Blog</a
-                    >
-                  </li>
-                  <li class="nav-item">
-                    <a
-                      href="#!"
-                      class="block px-4 pt-0 pb-1 pr-0 text-sm font-normal transition-colors ease-soft-in-out text-slate-500"
-                      target="_blank"
-                      >License</a
-                    >
-                  </li>
-                </ul>
-              </div>
-            </div>
+    <div fixed-plugin>
+      <a
+        fixed-plugin-button
+        class="bottom-7.5 right-7.5 text-xl z-990 shadow-soft-lg rounded-circle fixed cursor-pointer bg-white px-4 py-2 text-slate-700">
+        <i class="py-2 pointer-events-none fa fa-cog"> </i>
+      </a>
+      <!-- -right-90 in loc de 0-->
+      <div
+        fixed-plugin-card
+        class="z-sticky shadow-soft-3xl w-90 ease-soft -right-90 fixed top-0 left-auto flex h-full min-w-0 flex-col break-words rounded-none border-0 bg-white bg-clip-border px-2.5 duration-200">
+        <div class="px-6 pt-4 pb-0 mb-0 bg-white border-b-0 rounded-t-2xl">
+          <div class="float-left">
+            <h5 class="mt-4 mb-0">Soft UI Configurator</h5>
+            <p>See our dashboard options.</p>
           </div>
-        </footer>
+          <div class="float-right mt-6">
+            <button
+              fixed-plugin-close-button
+              class="inline-block p-0 mb-4 text-xs font-bold text-center uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none cursor-pointer hover:scale-102 leading-pro ease-soft-in tracking-tight-soft bg-150 bg-x-25 active:opacity-85 text-slate-700">
+              <i class="fa fa-close"></i>
+            </button>
+          </div>
+          <!-- End Toggle Button -->
+        </div>
+        <hr
+          class="h-px mx-0 my-1 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
+        <div class="flex-auto p-6 pt-0 sm:pt-4">
+          <!-- Sidebar Backgrounds -->
+          <div>
+            <h6 class="mb-0">Sidebar Colors</h6>
+          </div>
+          <a href="javascript:void(0)">
+            <div class="my-2 text-left" sidenav-colors>
+              <span
+                class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-purple-700 to-pink-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-slate-700 text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                active-color
+                data-color-from="purple-700"
+                data-color-to="pink-500"
+                onclick="sidebarColor(this)"></span>
+              <span
+                class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-gray-900 to-slate-800 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                data-color-from="gray-900"
+                data-color-to="slate-800"
+                onclick="sidebarColor(this)"></span>
+              <span
+                class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-blue-600 to-cyan-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                data-color-from="blue-600"
+                data-color-to="cyan-400"
+                onclick="sidebarColor(this)"></span>
+              <span
+                class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-green-600 to-lime-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                data-color-from="green-600"
+                data-color-to="lime-400"
+                onclick="sidebarColor(this)"></span>
+              <span
+                class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-red-500 to-yellow-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                data-color-from="red-500"
+                data-color-to="yellow-400"
+                onclick="sidebarColor(this)"></span>
+              <span
+                class="text-xs rounded-circle h-5.75 mr-1.25 w-5.75 ease-soft-in-out bg-gradient-to-tl from-red-600 to-rose-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                data-color-from="red-600"
+                data-color-to="rose-400"
+                onclick="sidebarColor(this)"></span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
